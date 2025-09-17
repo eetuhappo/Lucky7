7431a14ab0202f6b669a2fe93064a6164131a528
eetuhappo <eetu@happo.fi>
2025-09-17 23:25:46 +0300

    Lucky7 - peli versio1
    Satunnaisluvut ja pelin logiikka.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
         // Lucky7 harjoitus
+        
+        Random random = new Random();
+
+        // Muuttujat
+        int randomNumber1;
+        int randomNumber2;
+        int randomNumber3;
+
+        // arvotaan kolme numeroa väliltä 1-10
+        // jos jokin numero on 7 kerrotaan että voitit, muutoin että hävisit ja peli loppuu
+
+        randomNumber1 = random.nextInt(10)+1;
+        randomNumber2 = random.nextInt(10)+1;
+        randomNumber3 = random.nextInt(10)+1;
+        System.out.println("Arvotut numerot ovat: " + randomNumber1 + " " + randomNumber2 + " " + randomNumber3);
+
+        if (randomNumber1 == 7 || randomNumber2 == 7 || randomNumber3 == 7) {
+            System.out.println("Voitit!");
+        }
+        else {
+            System.out.println("Hävisit!");
+        }
+
     }
 }
