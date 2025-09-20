b5bf780f341e169f6551e22ed2a256103b86a6bd
eetuhappo <eetu@happo.fi>
2025-09-20 12:03:18 +0300

    Lucky7 - peli, versio 2
    Lisätty peliin rahat.
    - yksi kierros maksaa 1 euron.
    - 1x7 voittaa 2 euroa
    - 2x7 voittaa 5 euroa
    - 3x7 voittaa 10 euroa
    - peli loppuu kun rahat on loppu

--- src/App.java
+++ src/App.java
@@ -6,24 +6,43 @@ public class App {
         Random random = new Random();
 
         // Muuttujat
+        
         int randomNumber1;
         int randomNumber2;
         int randomNumber3;
-
+        int money = 5;
+        
+        // Pelin alussa on 5 euroa rahaa. Yksi kierros maksaa 1 euron.
         // arvotaan kolme numeroa väliltä 1-10
-        // jos jokin numero on 7 kerrotaan että voitit, muutoin että hävisit ja peli loppuu
+        // jos jokin numero on 7 voittaa 2 euroa, 2x7 voittaa 5 euroa ja 3x7 voittaa 10 euroa.
+        // Peli loppuu kun pelaajalla ei ole rahaa.
 
-        randomNumber1 = random.nextInt(10)+1;
-        randomNumber2 = random.nextInt(10)+1;
-        randomNumber3 = random.nextInt(10)+1;
-        System.out.println("Arvotut numerot ovat: " + randomNumber1 + " " + randomNumber2 + " " + randomNumber3);
+        System.out.println("Pelataan lucky 7 peliä. Yksi kierros maksaa 1 euron. Sinulla on rahaa " + money + " euroa");
+        do {
+            randomNumber1 = random.nextInt(10)+1;
+            randomNumber2 = random.nextInt(10)+1;
+            randomNumber3 = random.nextInt(10)+1;
+            money = money - 1;
+            System.out.println("Arvotut numerot ovat: " + randomNumber1 + " " + randomNumber2 + " " + randomNumber3);
 
-        if (randomNumber1 == 7 || randomNumber2 == 7 || randomNumber3 == 7) {
-            System.out.println("Voitit!");
+                
+            if (randomNumber1 == 7 && randomNumber2 == 7 && randomNumber3 == 7) {
+                money = money + 10;
+                System.out.println("Voitit 10 euroa. Sinulla on rahaa jäljellä " + money + " euroa.");
+            }
+                        else if (randomNumber1 == 7 && randomNumber2 == 7 || randomNumber1 == 7 && randomNumber3 == 7 || randomNumber2 == 7 && randomNumber3 == 7) {
+                money = money + 5;
+                System.out.println("Voitit 5 euroa. Sinulla on rahaa jäljellä " + money + " euroa.");
+            }
+                        else if (randomNumber1 == 7 || randomNumber2 == 7 || randomNumber3 == 7) {
+                money = money + 2;
+                System.out.println("Voitit 2 euroa. Sinulla on rahaa jäljellä " + money + " euroa.");
+            }
+            else {
+                System.out.println("Hävisit! Sinulla on rahaa jäljellä " + money + " euroa.");
+            }
         }
-        else {
-            System.out.println("Hävisit!");
-        }
-
+        while (money > 0);
+        System.out.println("Rahat loppu. Peli päättyi.");
     }
 }
